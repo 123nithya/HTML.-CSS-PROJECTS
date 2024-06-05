f046cb2e7ce3c7463b015c18dceda1ed52c8a0cc
Nithya Sivan <100074248+123nithya@users.noreply.github.com>
2024-06-05 12:30:12 +0530

    App.js

--- 100-Daya CSS Challenges/gym-landing-page_day-22/src/App.js
+++ 100-Daya CSS Challenges/gym-landing-page_day-22/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Navbar from './Components/Navbar';
-import HeroSection from './Components/HeroSection';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <HeroSection />
+      <Home />
     </div>
   );
 }
