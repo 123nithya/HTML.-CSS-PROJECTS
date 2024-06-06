1a68f86cd59b836578fe2215fb4f51f5e28facae
Nithya Sivan <100074248+123nithya@users.noreply.github.com>
2024-06-06 19:08:46 +0530

    Add files via upload

---
+++ 100-Daya CSS Challenges/day24/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BgVideo from './Components/BgVideo';
+
+function App() {
+  return (
+    <div className="App">
+      <BgVideo />
+    </div>
+  );
+}
+
+export default App;
